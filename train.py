912b74f1203b9aa33f665d6d2259c277f46ebc8c
Berge Maxim <44107024+GraphtyLove@users.noreply.github.com>
2021-10-18 09:28:37 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,21 @@
 from gan.model import DCGAN
 import torch
 
+print("GPU available: ", torch.cuda.is_available())
+
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print("device: ", device)
 nb_gpu = 1 if device.type == 'cuda' else 0
+print("number of GPU: ", nb_gpu)
+print("Defining model...")
 model = DCGAN(ngpu=nb_gpu)
+print("Start training mode...")
 model.fit(
-    epochs=1000,
-    dataset_path="data/dice/"
+    epochs=500,
+    batch_size=128,
+    dataset_path="data/dice/",
+    save_samples=True
 )
+print("Model saved.")
+model.save_metrics("/output/metrics.json")
+torch.save(model, "/output/model.pth")
